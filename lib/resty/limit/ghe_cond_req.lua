3bf5befa684d69028a5c35384fd7471f6069391e
Keshane Gan <keshanegan@gmail.com>
2017-01-19 15:20:50 -0600

    draft of conditional rate limiting

---
+++ lib/resty/limit/ghe_cond_req.lua
@@ -0,0 +1,177 @@
+local ngx_shared = ngx.shared
+local ngx_time = ngx.time
+local setmetatable = setmetatable
+local ffi_cast = ffi.cast
+local ffi_str = ffi.string
+local abs = math.abs
+local tonumber = tonumber
+local type = type
+local assert = assert
+
+-- TODO: we could avoid the tricky FFI cdata when lua_shared_dict supports
+-- hash-typed values as in redis.
+ffi.cdef[[
+    struct lua_resty_limit_ghe_req_rec {
+        unsigned int         remaining;  /* number of requests remaining */
+        uint64_t             reset;  /* time at which the window resets  */
+        /* integer value, 1 corresponds to 1 second */
+    };
+]]
+local const_rec_ptr_type = ffi.typeof("const struct lua_resty_limit_ghe_req_rec*")
+local rec_size = ffi.sizeof("struct lua_resty_limit_ghe_req_rec")
+
+-- we can share the cdata here since we only need it temporarily for
+-- serialization inside the shared dict:
+local rec_cdata = ffi.new("struct lua_resty_limit_ghe_req_rec")
+
+
+local _M = {
+    _VERSION = 'alpha'
+}
+
+
+local mt = {
+    __index = _M
+}
+
+-- the "limit" argument controls number of request allowed in a time window.
+-- time "window" argument controls the time window in seconds.
+function _M.new(dict_name, limit, window)
+    local dict = ngx_shared[dict_name]
+    if not dict then
+        return nil, "shared dict not found"
+    end
+
+    assert(limit > 0 and window > 0)
+
+    local self = {
+        dict = dict,
+        limit = limit,
+        window = window,
+    }
+
+    return setmetatable(self, mt)
+end
+
+
+-- sees an new incoming event
+-- the "commit" argument controls whether should we record the event in shm.
+-- FIXME we have a (small) race-condition window between dict:get() and
+-- dict:set() across multiple nginx worker processes. The size of the
+-- window is proportional to the number of workers.
+function _M.incoming(self, key, commit, conditional)
+    local dict = self.dict
+    local limit = self.limit
+    local window = self.window
+    local now = ngx_time()
+
+    local conditional
+
+    local remaining
+    local reset
+
+    -- 
+    -- it's important to anchor the string value for the read-only pointer
+    -- cdata:
+
+
+    local v = dict:get(key)
+    if v then
+        if type(v) ~= "string" or #v ~= rec_size then
+            return nil, "shdict abused by other users"
+        end
+        local rec = ffi_cast(const_rec_ptr_type, v)
+        local ttl = tonumber(rec.reset) - now
+
+        -- print("ttl: ", ttl, "s")
+
+        if ttl > 0 and conditional then
+            reset = tonumber(rec.reset)
+            remaining = tonumber(rec.remaining)
+        elseif ttl > 0 then
+            reset = tonumber(rec.reset)
+            remaining = tonumber(rec.remaining) - 1
+        else
+            reset = now + window
+            remaining = limit
+        end
+
+        -- print("remaining: ", remaining)
+
+        if remaining < 0 then
+            return -1, reset
+        end
+    elseif conditional then
+        remaining = limit 
+        reset = now + window
+
+    else
+        remaining = limit - 1
+        reset = now + window
+    end
+
+    if commit then
+        rec_cdata.remaining = remaining
+        rec_cdata.reset = reset
+        dict:set(key, ffi_str(rec_cdata, rec_size))
+    end
+
+    -- return remaining and time to reset
+    return remaining, reset
+end
+
+function _M.outgoing(self, key, modified)
+    local dict = self.dict
+    local limit = self.limit
+    local window = self.window
+    local now = ngx_time()
+
+
+    local v = dict:get(key)
+    if v and modified then
+        if type(v) ~= "string" or #v ~= rec_size then
+            return nil, "shdict abused by other users"
+        end
+        local rec = ffi_cast(const_rec_ptr_type, v)
+        local ttl = tonumber(rec.reset) - now
+
+        if ttl > 0 then
+            reset = tonumber(rec.reset)
+            remaining = tonumber(rec.remaining) - 1
+        else
+            reset = now + window
+            remaining = limit
+        end
+    elseif v and not modified then
+        if type(v) ~= "string" or #v ~= rec_size then
+            return nil, "shdict abused by other users"
+        end
+        local rec = ffi_cast(const_rec_ptr_type, v)
+        local ttl = tonumber(rec.reset) - now
+
+        --redundant when used with incoming?--
+        if ttl > 0 then
+            reset = tonumber(rec.reset)
+            remaining = tonumber(rec.remaining)
+        else
+            reset = now + window
+            remaining = limit
+        end
+
+    rec_cdata.remaining = remaining
+    rec_cdata.reset = reset
+    dict:set(key, ffi_str(rec_cdata, rec_size))
+
+    return remaining, reset
+end
+ 
+       
+
+
+
+
+
+
+return _M
+
+
---
+++ lib/resty/limit/ghe_cond_req.md
@@ -0,0 +1,45 @@
+```nginx
+http {
+    lua_shared_dict my_limit_req_store 100m;
+
+    server {
+        location / {
+            access_by_lua_block {
+                local limit_req = require "resty.limit.ghe_req"
+
+                -- rate: 5000 requests per 3600s
+                local lim, err = limit_req.new("api_limit_req_store", 5000, 3600)
+                if not lim then
+                    ngx.log(ngx.ERR, "failed to instantiate a resty.limit.req object: ", err)
+                    return ngx.exit(500)
+                end
+
+                local headers = ngx.req.get_headers()
+
+                local conditional = headers["If-None-Match"] or headers["If-Modified-Since"]
+                -- use the Authorization header as the limiting key
+                local key = headers["Authorization"] or "public"
+                local remaining, reset = lim:incoming(key, true, conditional)
+
+
+                if conditional and ngx.status == ngx.HTTP_NOT_MODIFIED then
+                    remaining, reset = lim:outgoing(key, false)
+                elseif conditional and ngx.status != ngx.HTTP_NOT_MODIFIED then
+                    remaining, reset = lim:outgoing(key, true) 
+
+
+                ngx.header["X-RateLimit-Limit"] = "5000"
+                ngx.header["X-RateLimit-Reset"] = reset
+
+                if remaining < 0 then
+                    ngx.header["X-RateLimit-Remaining"] = 0
+                    ngx.log(ngx.WARN, "rate limit exceeded")
+                    return ngx.exit(403)
+                else
+                    ngx.header["X-RateLimit-Remaining"] = remaining
+                end
+            }
+        }
+    }
+}
+```
